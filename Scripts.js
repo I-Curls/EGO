6c72261f3156a88eebadf735fa69d76a5db9c759
Caique <caique.matos@outlook.com>
2024-01-24 23:35:11 -0300

    Estrutura básica da página e início do script

---
+++ Scripts.js
@@ -0,0 +1,20 @@
+const gotEnding = [];
+const gotInfo = [];
+
+const option1 = document.querySelector('#option1');
+const option2 = document.querySelector('#option2');
+const option3 = document.querySelector('#option3');
+const option4 = document.querySelector('#option4');
+const option5 = document.querySelector('#option5');
+const option6 = document.querySelector('#option6');
+
+const text = document.querySelector("#mainText");
+
+const endings = [
+    { name: "Ditador", final: "final 1 de 5" },
+    { name: "Pacifista", final: "final 2 de 5" },
+    { name: "Limpeza Étnica", final: "final 3 de 5" },
+    { name: '"Democracia"', final: "final 4 de 5" },
+    { name: "Democracia!", final: "final 5 de 5" },
+]
+
