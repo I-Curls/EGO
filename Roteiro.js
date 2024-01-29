85a46c11273122cb0a2f7b82e97a2d1432eb4d29
Caique <caique.matos@outlook.com>
2024-01-28 21:17:34 -0300

    may dog have mercy on our souls

--- Roteiro.js
+++ Roteiro.js
@@ -1,1 +1,40 @@
-// Primeira Escolha
+export const eventoAtual = [
+    {
+        nome: "Intro",
+        text: "Você é basicamento o super-homem. Você consegue ver vários problemas no mundo e tem o poder para resolvê-los. Qual é a primera coisa que você quer fazer?",
+        "button text": ["Ir atrás de criminosos", "Não mostrar seus poderes publicamente", "Voar direto para Brasília (Capital do país)", "Procurar cientistas que podem ajudar a entender melhor seus poderes."],
+        "update button": [pathA, pathB, pathC, pathD]
+    },
+    {
+        nome: "test_pathA",
+        text: "Teste do caminho A: Ir atrás de criminosos",
+        "button text": ["Voltar ao início", "Não mostrar seus poderes publicamente", "Voar direto para Brasília (Capital do país)", "Procurar cientistas que podem ajudar a entender melhor seus poderes."],
+        "update button": [introTest, pathB, pathC, pathD]
+    },
+    {
+        nome: "test_pathB",
+        text: "Teste do caminho B: Não mostrar seus poderes publicamente",
+        "button text": ["Voltar ao início", "Ir atrás de criminosos", "Voar direto para Brasília (Capital do país)", "Procurar cientistas que podem ajudar a entender melhor seus poderes."],
+        "update button": [introTest, pathA, pathC, pathD]
+    },
+    {
+        nome: "test_pathC",
+        text: "Teste do caminho C: Voar direto para Brasília (Capital do país)",
+        "button text": ["Voltar ao início", "Não mostrar seus poderes publicamente", "Ir atrás de criminosos", "Procurar cientistas que podem ajudar a entender melhor seus poderes."],
+        "update button": [introTest, pathB, pathA, pathD]
+    },
+    {
+        nome: "test_pathD",
+        text: "Teste do caminho D: Procurar cientistas que podem ajudar a entender melhor seus poderes.",
+        "button text": ["Voltar ao início", "Não mostrar seus poderes publicamente", "Voar direto para Brasília (Capital do país)", "Ir atrás de criminosos"],
+        "update button": [introTest, pathB, pathC, pathA]
+    }
+];
+
+const endings = [
+    { name: "Ditador", final: "final 1 de 5" },
+    { name: "Pacifista", final: "final 2 de 5" },
+    { name: "Limpeza Étnica", final: "final 3 de 5" },
+    { name: '"Democracia"', final: "final 4 de 5" },
+    { name: "Democracia!", final: "final 5 de 5" },
+];
--- Scripts.js
+++ Scripts.js
@@ -1,3 +1,6 @@
+import { eventoAtual } from "./roteiro.js";
+
+let day = 1;
 const gotEnding = [];
 const gotInfo = [];
 
@@ -10,11 +13,53 @@ const option6 = document.querySelector('#option6');
 
 const text = document.querySelector("#mainText");
 
-const endings = [
-    { name: "Ditador", final: "final 1 de 5" },
-    { name: "Pacifista", final: "final 2 de 5" },
-    { name: "Limpeza Étnica", final: "final 3 de 5" },
-    { name: '"Democracia"', final: "final 4 de 5" },
-    { name: "Democracia!", final: "final 5 de 5" },
-]
+function introTest() {
+    startGame(eventoAtual[0]);
+    console.log(eventoAtual[0].update_button.length);
+};
+
+function pathA() {
+    startGame(eventoAtual[1]);
+};
+
+function pathB() {
+    startGame(eventoAtual[2]);
+};
+
+function pathC() {
+    startGame(eventoAtual[3]);
+};
+
+function pathD() {
+    startGame(eventoAtual[4]);
+};
+
+
+// Começa o jogo
+option1.onclick = introTest;
+
+// function atualizaEvento(eventoAtual) {
+//     for (let i = 0; i < eventoAtual.update_button.length; i++) {
+        
+        
+//     };
+    
+// };
+
+function startGame(evento) {
+    option2.style.display = "block";
+    option3.style.display = "block";
+    option4.style.display = "block";
+    option1.innerText = evento["button text"][0];
+    option2.innerText = evento["button text"][1];
+    option3.innerText = evento["button text"][2];
+    option4.innerText = evento["button text"][3];
+    option1.onclick = evento["update button"][0];
+    option2.onclick = evento["update button"][1];
+    option3.onclick = evento["update button"][2];
+    option4.onclick = evento["update button"][3];
+    text.innerText = evento["text"];
+
+    console.log(evento.update_button.length)
+};
 
